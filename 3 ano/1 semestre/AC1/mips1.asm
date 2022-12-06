ab99463d762e7ac1917c983713cafa375a786f07
fbarbosa2002 <75202920+fbarbosa2002@users.noreply.github.com>
2022-12-06 14:15:15 +0000

    .

--- 3 ano/1 semestre/AC1/mips1.asm
+++ 3 ano/1 semestre/AC1/mips1.asm
@@ -1,22 +1,17 @@
 
 .data
+L1: .asciiz "Aulas5&6T"
+L2: .byte 5,8,23
+L3: .word 5,8,23
+L4: .space 5
 
 
-X2:	.space 3
-
-X1:	.word 4
-X3:	.asciiz "ola"
 .text
+
 .globl main
 
-main:	la	$t0,X1
-	ori	$t1,$t0,0
-	xor	$t2,$t1,$t0
-	add	$t3,$t3,$t3
-	addu	$t3,$t3,$t3
-	addiu	$t3,$t3,0xFFFF
-	sub	$t3,$t3,3
-	lw $t3,0($t0)
-	ble	$t3,$t4,main
-	j main
-	jr $ra
+# i -> $2
+main: jr $ra
+
+	
+	
